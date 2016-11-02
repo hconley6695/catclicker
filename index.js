fe6d378b3569b51ea1b67b1f70134ce0ac8c9d1f
Heather Conley <hconley6695@gmail.com>
2016-11-01 22:10:17 -0400

    needs additional cat, with its own clicker, and am working on adding a name through a variable

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 (function () {
-	var num = 0;
-	
-	document.getElementById('click-img').addEventListener('click', catClick);
+	var num_one = 0;
+	var num_two = 0;
+	var name_one, name_two;
+
+	document.getElementById('click-img1').addEventListener('click', catClick);
+	document.getElementById('click-img2').addEventListener('click', secondCatClick);
 
 	function catClick() {
 		// alert('You clicked a cat.');
-		num++;
-		document.getElementById('click-number').innerHTML = num;
+		num_one++;
+		document.getElementById('click-number1').innerHTML = num_one;
+	}
+
+	function secondCatClick() {
+		num_two++;
+		document.getElementById('click-number2').innerHTML = num_two;
 	}
 
 
