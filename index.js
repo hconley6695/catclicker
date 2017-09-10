c817d658f5dfbac561d41549ef7e26169e729d09
Heather Conley <hconley6695@gmail.com>
2017-09-09 21:13:01 -0400

    refactored html to put all in list-item; refactored jquery to include the word this to reference each one.

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-(function () {
-	var num_one = 0;
-	var num_two = 0;
-	var name_one, name_two;
+// (function () {
+// 	var num_one = 0;
+// 	var num_two = 0;
+// 	var name_one, name_two;
 
-	document.getElementById('click-img1').addEventListener('click', catClick);
-	document.getElementById('click-img2').addEventListener('click', secondCatClick);
+// 	document.getElementById('click-img1').addEventListener('click', catClick);
+// 	document.getElementById('click-img2').addEventListener('click', secondCatClick);
 
-	function catClick() {
-		// alert('You clicked a cat.');
-		num_one++;
-		document.getElementById('click-number1').innerHTML = num_one;
-	}
+// 	function catClick() {
+// 		// alert('You clicked a cat.');
+// 		num_one++;
+// 		document.getElementById('click-number1').innerHTML = num_one;
+// 	}
 
-	function secondCatClick() {
-		num_two++;
-		document.getElementById('click-number2').innerHTML = num_two;
-	}
+// 	function secondCatClick() {
+// 		num_two++;
+// 		document.getElementById('click-number2').innerHTML = num_two;
+// 	}
 
 
 
-})();
+// })();
+
+jQuery( document ).ready(function() {
+		// var num_one = 0;
+		// var num_two = 0;
+		// var name_one, name_two;
+		var num = 0;
+
+		jQuery('li button.cat').on('click', function() {
+
+			num = $(this).siblings('span.outside-span').children('span.click-number').html();
+			console.log(num);
+			num++;
+
+			$(this).siblings('span.outside-span').children('span.click-number').html(num);
+		
+		});
+
+
+
+
+});
